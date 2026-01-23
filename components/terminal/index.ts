98118262506ebc911e7ecaabe0de7c8df5c52e62
Zachary Blevins <zachary.a.blevins@gmail.com>
2026-01-23 00:13:09 -0500

    chore(05-02): update barrel export with HistoryDisplay
    - Add HistoryDisplay component export
    - Add HistoryDisplayProps type export
    - Organize exports by category (components, syntax, types)

--- components/terminal/index.ts
+++ components/terminal/index.ts
@@ -1,4 +1,11 @@
+// Components
 export { TerminalLine } from './TerminalLine'
+export { HistoryDisplay } from './HistoryDisplay'
+
+// Syntax highlighting
 export { tokenizeInput, renderTokens, highlightInput, highlightLine } from './SyntaxHighlighter'
+
+// Types
 export type { Token, TokenType, TerminalLineProps } from './types'
+export type { HistoryDisplayProps } from './HistoryDisplay'
 export { VALID_COMMANDS, HEADER_KEYWORDS } from './types'
