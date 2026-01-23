38af3e021e1d7a290924e211cb27a5e71ee98af3
Zachary Blevins <zachary.a.blevins@gmail.com>
2026-01-23 00:13:50 -0500

    feat(05-03): export InputLine from barrel
    - Add InputLine component export
    - Add InputLineHandle and InputLineProps type exports

--- components/terminal/index.ts
+++ components/terminal/index.ts
@@ -1,6 +1,7 @@
 // Components
 export { TerminalLine } from './TerminalLine'
 export { HistoryDisplay } from './HistoryDisplay'
+export { InputLine } from './InputLine'
 
 // Syntax highlighting
 export { tokenizeInput, renderTokens, highlightInput, highlightLine } from './SyntaxHighlighter'
@@ -8,4 +9,5 @@ export { tokenizeInput, renderTokens, highlightInput, highlightLine } from './Sy
 // Types
 export type { Token, TokenType, TerminalLineProps } from './types'
 export type { HistoryDisplayProps } from './HistoryDisplay'
+export type { InputLineHandle, InputLineProps } from './InputLine'
 export { VALID_COMMANDS, HEADER_KEYWORDS } from './types'
